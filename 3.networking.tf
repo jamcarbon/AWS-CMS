4da0448325fd112a33c3e42c97dc91605cc50b03
David <jamcarbon@hotmail.com>
2022-09-06 19:12:53 +1200

    update

--- 3.networking.tf
+++ 3.networking.tf
@@ -16,26 +16,21 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_subnet" "private-us-east-1a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    "Name"                            = "private-us-east-1a"
-  }
-}
 
 # Public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   count                   = length(var.public_subnets_cidr)
   cidr_block              = element(var.public_subnets_cidr, count.index)
-  availability_zone       = element(var.availability_zones, count.index)
+  availability_zone       = [
+    "${var.region}a",
+    "${var.region}b",
+    "${var.region}c",
+  ]
   map_public_ip_on_launch = true
 
   tags = {
-    Name        = "${var.project_name}-${element(var.availability_zones, count.index)}-public-subnet"
+    Name        = "${var.project_name}-public-subnet"
   }
 }
 
@@ -44,10 +39,14 @@ resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main.id
   count                   = length(var.private_subnets_cidr)
   cidr_block              = element(var.private_subnets_cidr, count.index)
-  availability_zone       = element(var.availability_zones, count.index)
+  availability_zone       = [
+    "${var.region}a",
+    "${var.region}b",
+    "${var.region}c",
+  ]
   map_public_ip_on_launch = false
 
   tags = {
-    Name        = "${var.project_name}-${element(var.availability_zones, count.index)}-private-subnet"
+    Name        = "${var.project_name}-private-subnet"
   }
 }
