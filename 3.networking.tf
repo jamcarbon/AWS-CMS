7a84f7da777ad89788477d665897c9fd5c59b082
David <jamcarbon@hotmail.com>
2022-09-06 19:46:44 +1200

    update

--- 3.networking.tf
+++ 3.networking.tf
@@ -22,11 +22,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   count                   = length(var.public_subnets_cidr)
   cidr_block              = element(var.public_subnets_cidr, count.index)
-  availability_zone       = [
-    "${var.region}a",
-    "${var.region}b",
-    "${var.region}c",
-  ]
+  
   map_public_ip_on_launch = true
 
   tags = {
@@ -39,11 +35,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main.id
   count                   = length(var.private_subnets_cidr)
   cidr_block              = element(var.private_subnets_cidr, count.index)
-  availability_zone       = [
-    "${var.region}a",
-    "${var.region}b",
-    "${var.region}c",
-  ]
+  
   map_public_ip_on_launch = false
 
   tags = {
