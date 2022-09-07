c273f109deeed2e8f1c0fbf990ec23df7b1d800b
jamcarbon <jamcarbon@gmail.com>
2022-09-08 00:20:02 +1200

    update

--- terraform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter your environment (staging or prod)"
-read environment
-
-rm -Rf .terraform/
-
-terraform init -upgrade=true -var-file=vars/$environment.tfvars
-#terraform apply -var-file=vars/$environment.tfvars
