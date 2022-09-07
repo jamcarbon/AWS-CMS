ccb3be05b5ff772fe6534df08095c0bc710aa182
David <jamcarbon@hotmail.com>
2022-09-08 00:04:15 +1200

    update script

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo yum install httpd -y
-sudo systemctl start httpd
+#connect the RDS database
 mysql -h terraform-20220907055625034600000003.czalq65fdmds.ap-south-1.rds.amazonaws.com -u root -p$w#CffEeoXNaG mydb
